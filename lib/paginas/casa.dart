93d5dabfb2421cccea998e7f24ff030703060ff1
Irene Barboteo <a_irene.barboteo.company@iespablopicasso.es>
2023-05-08 13:07:18 +0200

    Creado el filtro

--- lib/paginas/casa.dart
+++ lib/paginas/casa.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hambrout/utils/formularios.dart';
 
 import '../firebase/conexion_firebase.dart';
+import '../utils/formularios.dart';
 
 class CasaWidget extends StatefulWidget{
   const CasaWidget({super.key});
@@ -14,12 +14,28 @@ class CasaWidget extends StatefulWidget{
 
 final ConexionDatos conexionDatos = ConexionDatos();
 
+
+
 class _Casa extends State<CasaWidget>{
+
+  late List<ElevatedButton> _botones = [];
 
   final List<Widget> _widgets = [];
   late List recetas=[];
 
-  final String btnPulsado='todo';
+  late String btnPulsado='todo';
+
+  @override
+  void initState() {
+    _botones = [
+      ElevatedButton(onPressed: (){btnPulsado='todo'; cambiarRecetas(); setState((){});}, child: Text('Todo')),
+      ElevatedButton(onPressed: (){btnPulsado='España'; cambiarRecetas();setState((){});}, child: Text('España')),
+      ElevatedButton(onPressed: (){btnPulsado='Rumania'; cambiarRecetas();}, child: Text('Rumanía')),
+      ElevatedButton(onPressed: (){btnPulsado='marruecos'; cambiarRecetas();}, child: Text('Marruecos')),
+      ElevatedButton(onPressed: (){btnPulsado='eeuu'; cambiarRecetas();}, child: Text('EE.UU')),
+      ElevatedButton(onPressed: (){btnPulsado='japon'; cambiarRecetas();}, child: Text('Japón')),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +44,7 @@ class _Casa extends State<CasaWidget>{
 
     return
         ListView(
-          shrinkWrap: true,
+          //shrinkWrap: true,
           padding: const EdgeInsets.all(40),
           scrollDirection: Axis.vertical,
           children: [
@@ -40,9 +56,26 @@ class _Casa extends State<CasaWidget>{
               ],
             ),
             formatosDisenio.separacionNormal(context),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('listview horizontal que muestra el origen'),],
+            Wrap(
+              
+            ),
+            Container(
+              height: 80,
+              child: ListView.builder(
+                itemCount: _botones.length,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index){
+                  return Row(
+                    children: [
+                      Column(
+                        children: [_botones[index]],
+                      ),
+                      const SizedBox(width: 10,)
+                    ],
+                  );
+                },
+              ),
             ),
             formatosDisenio.separacionNormal(context),
             FutureBuilder(
@@ -71,14 +104,20 @@ class _Casa extends State<CasaWidget>{
 
   //EL SET STATE SE HACE EN EL ONPRESSED DE LOS BOTONES
 
-  Future<List>? cambiarRecetas(){
+  Future<List?>? cambiarRecetas()async {
+    recetas = await conexionDatos.buscarRecetas();
+    print('------------------\n$recetas');
     if(btnPulsado=='todo'){
-      return conexionDatos.buscarRecetas();
+      return recetas;
     }
-    return null;
+    List recetasActuales = [];
+    for(var receta in recetas){
+      if(receta['origen']==btnPulsado){
+        recetasActuales.add(receta);
+      }
+    }
+    return recetasActuales;
   }
-
-
 
 /**
  * FUNCION QUE TIENE QUE HACER VARIAS COSAS
