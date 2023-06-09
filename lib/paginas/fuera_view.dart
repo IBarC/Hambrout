0551eb39dc96a8b178775e692499d8d34c2c6159
Irene Barboteo <a_irene.barboteo.company@iespablopicasso.es>
2023-06-09 18:50:47 +0200

    El mapa se centra al pulsar en una localizacion

--- lib/paginas/fuera_view.dart
+++ lib/paginas/fuera_view.dart
@@ -59,8 +59,8 @@ class FueraState extends State<FueraWidget>{
   @override
   void initState() {
     super.initState();
-    pageController = PageController(
-      initialPage: 1, viewportFraction: 0.85)..addListener(alDeslizar);
+    //pageController = PageController(
+      //initialPage: 1, viewportFraction: 0.85)..addListener(alDeslizar);
   }
 
   void alDeslizar(){
@@ -138,7 +138,7 @@ class FueraState extends State<FueraWidget>{
     return LatLng(lat, long);
   }
 
-  void setLugaresMarker(LatLng pos, String label, List tipos, String estado) async{
+  void setLugaresMarker(LatLng pos, String label, List tipos, String estado) {
     var counter = markerIdCounter++;
 
     final Marker marker;
@@ -150,8 +150,9 @@ class FueraState extends State<FueraWidget>{
         position: pos,
         onTap: (){},
         icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-        infoWindow: InfoWindow(title: label, onTap: (){
-
+        infoWindow: InfoWindow(title: label, onTap: () async {
+          GoogleMapController controlador = await _googleMapController.future;
+          controlador.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: pos,zoom: 20)));
         })
       );
       setState(() {
@@ -243,20 +244,7 @@ class FueraState extends State<FueraWidget>{
               ),
             ),
           ),
-          Positioned(
-            bottom: 20.0,
-            child: Container(
-              height: 200,
-              width: MediaQuery.of(context).size.width,
-              child: PageView.builder(
-                controller: pageController,
-                itemCount: allLugares.length,
-                itemBuilder: (BuildContext context, int index){
-                  return nearbyPlacesList(index);
-                },
-              ),
-            ),
-          )
+
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -343,152 +331,6 @@ class FueraState extends State<FueraWidget>{
         zoom: 14.0,
         bearing: 45.0,
         tilt: 45.0)));
-  }
-
-  nearbyPlacesList(index) {
-    return AnimatedBuilder(
-      animation: pageController,
-      builder: (BuildContext context, Widget? widget) {
-        double value = 1;
-        if (pageController.position.haveDimensions) {
-          value = (pageController.page! - index);
-          value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
-        }
-        return Center(
-          child: SizedBox(
-            height: Curves.easeInOut.transform(value) * 125.0,
-            width: Curves.easeInOut.transform(value) * 350.0,
-            child: widget,
-          ),
-        );
-      },
-      child: InkWell(
-        onTap: () async {
-          cardTapped = !cardTapped;
-          if (cardTapped) {
-            tappedPlaceDetail = await MapServices()
-                .getPlace(allLugares[index]['place_id']);
-            setState(() {});
-          }
-          moveCameraSlightly();
-        },
-        child: Stack(
-          children: [
-            Center(
-              child: Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 10.0,
-                  vertical: 20.0,
-                ),
-                height: 125.0,
-                width: 275.0,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black54,
-                          offset: Offset(0.0, 4.0),
-                          blurRadius: 10.0)
-                    ]),
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Colors.white),
-                  child: Row(
-                    children: [
-                      pageController.position.haveDimensions
-                          ? pageController.page!.toInt() == index
-                          ? Container(
-                        height: 90.0,
-                        width: 90.0,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(10.0),
-                              topLeft: Radius.circular(10.0),
-                            ),
-                            image: DecorationImage(
-                                image: NetworkImage(imagenSitio != ''
-                                    ? 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=$imagenSitio&key=$key'
-                                    : 'https://pic.onlinewebfonts.com/svg/img_546302.png'),
-                                fit: BoxFit.cover)),
-                      )
-                          : Container(
-                        height: 90.0,
-                        width: 20.0,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(10.0),
-                              topLeft: Radius.circular(10.0),
-                            ),
-                            color: Colors.blue),
-                      )
-                          : Container(),
-                      SizedBox(width: 5.0),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 170.0,
-                            child: Text(allLugares[index]['name'],
-                                style: TextStyle(
-                                    fontSize: 12.5,
-                                    fontFamily: 'WorkSans',
-                                    fontWeight: FontWeight.bold)),
-                          ),
-                          /**RatingStars(
-                            value: allLugares[index]['rating']
-                                .runtimeType ==
-                                int
-                                ? allLugares[index]['rating'] * 1.0
-                                : allLugares[index]['rating'] ?? 0.0,
-                            starCount: 5,
-                            starSize: 10,
-                            valueLabelColor: const Color(0xff9b9b9b),
-                            valueLabelTextStyle: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'WorkSans',
-                                fontWeight: FontWeight.w400,
-                                fontStyle: FontStyle.normal,
-                                fontSize: 12.0),
-                            valueLabelRadius: 10,
-                            maxValue: 5,
-                            starSpacing: 2,
-                            maxValueVisibility: false,
-                            valueLabelVisibility: true,
-                            animationDuration: Duration(milliseconds: 1000),
-                            valueLabelPadding: const EdgeInsets.symmetric(
-                                vertical: 1, horizontal: 8),
-                            valueLabelMargin: const EdgeInsets.only(right: 8),
-                            starOffColor: const Color(0xffe7e8ea),
-                            starColor: Colors.yellow,
-                          ),**/
-                          Container(
-                            width: 170.0,
-                            child: Text(
-                              allLugares[index]['business_status'] ??
-                                  'none',
-                              style: TextStyle(
-                                  color: allLugares[index]
-                                  ['business_status'] ==
-                                      'OPERATIONAL'
-                                      ? Colors.green
-                                      : Colors.red,
-                                  fontSize: 11.0,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
   }
 
   void addMarker(String markerId, LatLng pos){
